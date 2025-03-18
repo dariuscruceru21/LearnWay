8ff3ecf51ba849e4ae1e34662aad49b26024443f
Darius Cruceru <dariuscruceru@192.168.1.83>
2025-03-18 18:54:13 +0200

    finished setting up drizzle and neon postgresql, created user schema and test connection to database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,23 @@
 import CourseList from "@/components/CourseList";
- import CourseOverview from "@/components/CourseOverview";
+import CourseOverview from "@/components/CourseOverview";
 import { sampleCourses } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
-  <>
-    <CourseOverview {...sampleCourses[0]} />
-    <CourseList
-      title="Latest Courses"
-      courses={sampleCourses}
-      containerClassName="mt-28"
-    />
-  </>
-);
+const Home = async () => {
+  const result = await db.select().from(users)
+  console.log(JSON.stringify(result, null, 2));
+  return (
+    <>
+      <CourseOverview {...sampleCourses[0]} />
+
+      <CourseList
+        title="Latest Courses"
+        courses={sampleCourses}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
 
 export default Home;
